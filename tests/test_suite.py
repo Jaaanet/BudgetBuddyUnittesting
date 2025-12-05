3db6ed221e41b835f3ed874a63939b35781e73f4
Rachel <rmkamp@student.ubc.ca>
2025-12-04 19:34:18 -0800

    add budget and models to test suite

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -6,13 +6,15 @@ from .test_repository import TestRepository
 from .test_csvio import TestCsvIO
 from .test_summary import TestSummary
 from .test_main import TestBudgetBuddyApp
+from .test_budget import TestBudget
+from .test_models import TestUserProfile
 
 
 def suite():
     loader = unittest.TestLoader()
     test_suite = unittest.TestSuite()
 
-    for test_class in (TestRepository, TestCsvIO, TestSummary, TestBudgetBuddyApp):
+    for test_class in (TestRepository, TestCsvIO, TestSummary, TestBudgetBuddyApp, TestBudget, TestUserProfile):
         test_suite.addTests(loader.loadTestsFromTestCase(test_class))
 
     return test_suite
